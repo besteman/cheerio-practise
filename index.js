58369ac85413e0ed4abf62ad71370135d67b64de
Justin Besteman <justin@besteman.io>
2017-02-02 13:49:40 -0500

    starting to make routes

--- index.js
+++ index.js
@@ -1,49 +1,19 @@
-var cheerio = require('cheerio');
-var request = require('request');
-require('console.table');
+const express = require('express');
+const body_parser = require('body-parser');
+const method_override = require("method-override");
 
-request({
-    method: 'GET',
-    url: 'http://www.mywipersize.com/make/chevrolet/malibu.php'
-}, function(err, response, body) {
-    if (err) return console.error(err);
+const app = epress;
 
-    // Tell Cherrio to load the HTML
-    $ = cheerio.load(body);
-    
-   var test = $(".views-table").text();
+const port = 9000;
 
-   var array = test.split(" ");
+app.use(express.static(process.cwd() + "/public"));
 
-   //console.log(array);
+app.use(bodyParser.urlencoded({ extended: false }));
 
-   array = array.filter(function(entry) { return entry.trim() != ''; });
-
-   var test_array = [];
-
-	for (let i = 6; i < array.length; i = i + 3) {
-
-		let new_array = [];
-
-		new_array.push(array[i],array[i+1],array[i+2]);
-
-		test_array.push(new_array);
-
-	}
-
-	var cars = test_array.map(function (item) {
-	    return {
-	        year: parseInt(item[0], 10),
-	        driver_side: item[1],
-	        pass_side: item[2]
-	    };
-	});
-
-	console.log(cars);
-
-  //console.log(array);
+// Override with POST having ?_method=DELETE
+app.use(methodOverride("_method"));
 
 
-  console.table(cars);
-
+app.listen(port, function() {
+  console.log("Listening on PORT " + port);
 });

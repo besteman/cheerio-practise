b17334ef8fe1e69052db79f1a40dd2944d1bcfc3
Justin Besteman <justin@besteman.io>
2017-02-04 09:52:02 -0500

    Finished the client side just need to pretty it up

--- index.js
+++ index.js
@@ -2,17 +2,21 @@ const express = require('express');
 const body_parser = require('body-parser');
 const method_override = require("method-override");
 
-const app = epress;
+const app = express();
 
-const port = 9000;
+const port = 9001;
 
 app.use(express.static(process.cwd() + "/public"));
 
-app.use(bodyParser.urlencoded({ extended: false }));
-
+app.use(body_parser.json());
+app.use(body_parser.urlencoded({ extended: true }));
+app.use(body_parser.text());
+app.use(body_parser.json({ type: "application/vnd.api+json" }));
 // Override with POST having ?_method=DELETE
-app.use(methodOverride("_method"));
+app.use(method_override("_method"));
 
+require("./routes/api-route.js")(app);
+require("./routes/html-route.js")(app);
 
 app.listen(port, function() {
   console.log("Listening on PORT " + port);
